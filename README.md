50a8585ddbf2ea14dba4949d994e552ef5fe6ac9
Mauro Monti <monti.mauro@gmail.com>
2017-06-27 13:14:15 -0700

    Added documentation

--- README.md
+++ README.md
@@ -1,17 +1,14 @@
 ## Overview
 
-By default the test runs using the embeeded driver. If you want to browse the store trough the UI, use the http driver instead.
-### Running Neo4j in a Docker Container
- 
- ```
- docker run -p 7474:7474 -p 7687:7687 -v $HOME/Development/mount/neo4j -e NEO4J_AUTH=none neo4j
- ```
- 
+By default the test runs using the embedded driver. If you want to browse the store trough the UI, use the http driver instead.
+
 ### Using the Http Driver
  
 Replace the properties in: 
 
-_/test/resources/application-test.properties_ 
+```
+/test/resources/application-test.properties 
+```
 
 with the following key values:
 
@@ -19,3 +16,10 @@ with the following key values:
  wfg.neo4j.driver-classname=org.neo4j.ogm.drivers.http.driver.HttpDriver
  wfg.neo4j.uri=http://localhost:7474
 ```
+
+### Running Neo4j in a Docker Container
+ 
+ ```
+ docker run -p 7474:7474 -p 7687:7687 -v $HOME/Development/mount/neo4j -e NEO4J_AUTH=none neo4j
+ ```
+ 
--- src/test/java/com/dreamworks/example/TaskServiceTests.java
+++ src/test/java/com/dreamworks/example/TaskServiceTests.java
@@ -21,7 +21,6 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
-
 @Slf4j
 @ActiveProfiles("test")
 @ContextConfiguration(classes = { Neo4jEmbeddedConfig.class })
@@ -49,7 +48,7 @@ public class TaskServiceTests {
     }
 
     @Test
-    public void testTxIssue() throws Exception {
+    public void testIssue1() throws Exception {
         // = We create a task.
         Task task = taskService.create(new TaskPayload("task"));
 
@@ -96,5 +95,121 @@ public class TaskServiceTests {
 
         // = Ensure there are no children versions in version2
         Assert.assertEquals(0, storedTaskVersion2.getChilds().size());
+    }
+
+    @Test
+    public void testIssue2() throws Exception {
+        // = We create a task.
+        Task task = taskService.create(new TaskPayload("task"));
+
+        // = We create a task version and we establish the relation with the task.
+        TaskVersion version1 = taskService.create(task.getId(), null, new TaskVersionPayload());
+
+        // = We create a new task linked to the previous task version (version1) and also linked to the task.
+        TaskVersion version2 = taskService.create(task.getId(), version1.getId(), new TaskVersionPayload());
+
+        // = We create a new task linked to the previous task version (version2) and also linked to the task.
+        TaskVersion version3 = taskService.create(task.getId(), version2.getId(), new TaskVersionPayload());
+
+        // ====== Asserts on created objects. ======= //
+
+        // = Ensure parent is wired up correctly.
+        Assert.assertEquals(version2, version3.getParent());
+
+        // = Ensure the version 2 and 3 has only 1 children.
+
+        // FIXME: 6/27/17
+        // version1 has 2 children instead of 1 (commenting out this assert)
+        // Assert.assertEquals(1, version2.getParent().getChilds().size());
+
+        // FIXME: 6/27/17
+        // version2 has 2 children instead of 1 (commenting out this assert)
+        // Assert.assertEquals(1, version3.getParent().getChilds().size());
+
+        // = Ensure the version 3 doesn't have any children.
+        Assert.assertEquals(0, version3.getChilds().size());
+
+        // These asserts are Ok cause we are inspecting the objects we have in memory.
+        // Lets see what was saved in the Store...
+
+        // ======= Retrieve what is in the store and Asserts again with the same checks. ======= //
+
+        // = Get the task by ID (This is our generated ID and not the @GraphId).
+        Task storedTask = taskService.get(task.getId());
+
+        // = Ensure the the task has 3 versions
+        Assert.assertTrue(storedTask.hasVersions());
+        Assert.assertEquals(3, storedTask.getVersions().size());
+
+        // = Lets get what is in the store.
+        TaskVersion storedTaskVersion3 = taskVersionService.get(version3.getId());
+
+        // = Check that parent of version3 has a child.
+        Assert.assertEquals(1, storedTaskVersion3.getParent().getChilds().size());
+
+        // FIXME: 6/27/17
+        // = Ensure there are no children versions in version2
+        // Assert.assertEquals(0, storedTaskVersion3.getChilds().size());
+    }
+
+    @Test
+    public void testIssue3() throws Exception {
+        // = We create a task.
+        Task task = taskService.create(new TaskPayload("task"));
+
+        // = We create a task version and we establish the relation with the task.
+        TaskVersion version1 = taskService.create(task.getId(), null, new TaskVersionPayload());
+
+        // = We create a new task linked to the previous task version (version1) and also linked to the task.
+        TaskVersion version2 = taskService.create(task.getId(), version1.getId(), new TaskVersionPayload());
+
+        // = We create a new task linked to the previous task version (version2) and also linked to the task.
+        TaskVersion version3 = taskService.create(task.getId(), version2.getId(), new TaskVersionPayload());
+
+        // = We create a new task linked to the previous task version (version3) and also linked to the task.
+        TaskVersion version4 = taskService.create(task.getId(), version3.getId(), new TaskVersionPayload());
+
+        // ====== Asserts on created objects. ======= //
+
+        // = Ensure parent is wired up correctly.
+        Assert.assertEquals(version3, version4.getParent());
+
+        // = Ensure the version 2 and 3 has only 1 children.
+
+        // FIXME: 6/27/17
+        // version2 now has 2 children instead of 1 (commenting out this assert)
+        // Assert.assertEquals(1, version3.getParent().getChilds().size());
+
+        // FIXME: 6/27/17
+        // version3 now has 2 children instead of 1 (commenting out this assert)
+        // Assert.assertEquals(1, version4.getParent().getChilds().size());
+
+        // = Ensure the version 4 doesn't have any children.
+        Assert.assertEquals(0, version4.getChilds().size());
+
+        // These asserts are Ok cause we are inspecting the objects we have in memory.
+        // Lets see what was saved in the Store...
+
+        // ======= Retrieve what is in the store and Asserts again with the same checks. ======= //
+
+        // = Get the task by ID (This is our generated ID and not the @GraphId).
+        Task storedTask = taskService.get(task.getId());
+
+        // = Ensure the the task has 3 versions
+        Assert.assertTrue(storedTask.hasVersions());
+        Assert.assertEquals(4, storedTask.getVersions().size());
+
+        // = Lets get what is in the store.
+        TaskVersion storedTaskVersion4 = taskVersionService.get(version4.getId());
+
+        // = Check that parent of version4 has a child.
+        // FIXME: 6/27/17
+        // version3 now has 2 children instead of 1 (commenting out this assert)
+        // Assert.assertEquals(1, storedTaskVersion4.getParent().getChilds().size());
+
+        // = Ensure there are no children versions in version4
+        // FIXME: 6/27/17
+        // version4 shouldn't have any children.
+        Assert.assertEquals(0, storedTaskVersion4.getChilds().size());
     }
 }
